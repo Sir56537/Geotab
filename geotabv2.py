5d097efc11322e8372912210fb7e591035268684
Arthur <Sir*kitten566537>
2021-04-27 09:33:54 -0400

    fixed issues with nan values

--- geotabv2.py
+++ geotabv2.py
@@ -67,7 +67,7 @@ elif operator == "1":
 
 file_loc = "Raw.xlsx"
 df = pdf = pd.read_excel(file_loc, sheet_name='Summary',
-                         usecols="A,L", skiprows=10, NAN="~")
+                         usecols="A,L", skiprows=10)
 
 df.fillna('', inplace=True)
 
